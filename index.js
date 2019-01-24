14be823a76b9911cf8c181f16841ab30da5fd110
john.so <john.so@koreapolyschool.com>
2019-01-24 16:40:13 +0900

    20190124 16:40

--- index.js
+++ index.js
@@ -2,100 +2,89 @@
 process.setMaxListeners(0);
 const Alexa = require('alexa-sdk');
 const WebSocket = require('ws');
-const websocketserverurl = 'ws://54.180.21.15:15234/';
+const websocketserverurl = 'ws://54.180.21.15:7979/';
 
 
 const handlers = {
     'LaunchRequest': function () {
         console.log(`######LanchRequest######`);
-        this.emit(':tell',`say like this! ask power point open!`);
+        this.emit(':tell',`say like this! ask poly talk text John that I love you!`);
     },
 
+    'sendall': function(){
 
-    'openppt':function(){
-        console.log(`#####ppt-open######`);
+        var something= this.event.request.intent.slots.asq.value;
         let ws = new WebSocket(websocketserverurl);
+
 		ws.on('open', function open() {
-            ws.send('open');
-            //여기서 메시지도 받을수있나 테스트 해봐야함
+            console.log(`7979에 보냄:`,something);
+            ws.send(something);
         });
 
-        ws.on('message',function message(message){
-            
-            if(message.indexOf('PING')==-1){
-            ws.close();
-            this.emit(':tell',message);
-            }
-        }.bind(this));
-
-      
-       
-    },
-    
-
-
-    'pptclose':function(){
-        console.log(`#####ppt-close######`);
-        let ws = new WebSocket(websocketserverurl);
-		ws.on('open', function open() {
-			ws.send('close');
-        });
-
-
-        ws.on('message',function message(message){
-            if(message.indexOf('PING')==-1){
-            ws.close();
-            this.emit(':tell',message);
-            }
-        }.bind(this));
-        
-    },
-
-    'pptnextslide': function () {
-        console.log(`######pptnextslide######`);
-		let ws = new WebSocket(websocketserverurl);
-		ws.on('open', function open() {
-			ws.send('next');
-		
-        });
-        ws.on('message',function message(message){
-            if(message.indexOf('PING')==-1){
-            ws.close();
-            this.emit(':tell',message);
-            }
-        }.bind(this));
-        
-    },
-
-	'pptprevslide': function () {
-	
-        console.log(`######pptprevslide######`);
-		let ws = new WebSocket(websocketserverurl);
-		ws.on('open', function open() {
-			ws.send('previous');
-        });		
-        ws.on('message',function message(message){
+        ws.on('message',function message(message){            
             if(message.indexOf('PING')==-1){
                 ws.close();
+                console.log(`7979에서 받음:`,message);
                 this.emit(':tell',message);
             }
         }.bind(this));
+            
+         //this.emit(':tell',`Your message that said ${something} has been successfully sent to your selected contacts.`);  
     },
-
-	'ppttoslide': function () {
-        console.log(`######ppttovslide######`);
-        var sn=this.event.request.intent.slots.slidenumber.value;
+/*
+    'sendmessageall': function(){
+  
+      var anything = this.event.request.intent.slots.anything.value;
+      var username =this.event.request.intent.slots.user.value;
      
-        let ws = new WebSocket(websocketserverurl);
-		ws.on('open', function open() {
-			ws.send('slide=' + sn);
-        });
-        ws.on('message',function message(message){
-            if(message.indexOf('PING')==-1){
-            ws.close();
-            this.emit(':tell',message);
+        console.log(`anything:`,anything);
+        console.log(`username:`,username);
+        if(!anything || !username)
+        {
+            this.emit(':tell',`say like this! ask poly talk text John that I love you`);
+        }
+        else
+        {
+            username = username.toLowerCase();
+            if(username=='everybody')
+            {
+                for(var i = 0 ; i <npdata.length ; i++)
+                {
+                    // sendmessage(anything,npdata[i].phonenumber);
+                }
+                this.emit(':tell',`Your message that said ${anything} has been successfully sent to ${username}.`);  
             }
-        }.bind(this));
+            else
+            {
+                var phonenumber=`0`;
+                console.log(`npdata:`,npdata);
+                console.log(`npdata.length:`,npdata.length);
+                for(var i = 0 ; i <npdata.length ; i++)
+                {
+                    if(npdata[i].name ==username)
+                    {
+                        phonenumber=npdata[i].phonenumber;
+                        break;
+                    }
+                }
+                if(phonenumber!='0')
+                {
+                    //sendmessage(anything,phonenumber);
+                    this.emit(':tell',`Your message that said ${anything} has been successfully sent to ${username}.`);  
+                }
+                else
+                {
+                      this.emit(':tell',`poly talk can't find name ${username}.`);    
+                }
+             
+              
+            }
+        }
+    },
+    */
+
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell',`bye`);
     },
 
     'AMAZON.FallbackIntent': function () {
@@ -115,7 +104,7 @@ const handlers = {
 };
 
 
-var APP_ID = 'amzn1.ask.skill.03a27e50-52d9-40ed-852b-7dabac9eaed3'; //operation server
+var APP_ID = 'amzn1.ask.skill.eeada234-5c75-4af5-b968-d7d11abd449d';  //operation server
 exports.handler = (event, context,callback) => {
     const alexa = Alexa.handler(event, context,callback);
     alexa.appId = APP_ID;
